25f7e3f0f9a53e1780a4fbf96127478e4745a1ec
Tim de Putter <tim.de.putter83@googlemail.com>
2015-04-16 22:23:50 +0200

    refactoring

--- lib/rendezvous.ex
+++ lib/rendezvous.ex
@@ -1,7 +1,12 @@
 defmodule Rendezvous do
   def get algorithm, key, buckets do
-    {bucket, hash} = Enum.map(buckets, fn(b) -> {b, compute_bucket_hash(algorithm,b,key)} end) |> Enum.max_by fn {b, hash} -> hash end
+    {bucket, hash} = Enum.map(buckets, fn(b) -> get_bucket_with_hash(algorithm, b, key) end) 
+                      |> Enum.max_by fn {b, hash} -> hash end
     bucket
+  end
+
+  defp get_bucket_with_hash(algorithm, bucket, key) do
+    {bucket, compute_bucket_hash(algorithm, bucket, key)}
   end
 
   defp compute_bucket_hash(algorithm, bucket,key) do
