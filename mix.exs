c227939cff788926f37d78d98aea96a6fd616282
Tim de Putter <tim.de.putter83@googlemail.com>
2015-04-18 14:59:48 +0200

    Publish as Hex Package

--- mix.exs
+++ mix.exs
@@ -4,27 +4,20 @@ defmodule Rendezvous.Mixfile do
   def project do
     [app: :rendezvous,
      version: "0.0.1",
+     description: "Implementation of the Rendezvous or Highest Random Weight (HRW) hashing algorithm",
      elixir: "~> 1.0.0",
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
-  def application do
-    [applications: [:logger]]
-  end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     contributors: ["Tim de Putter"],
+     licenses: ["The MIT License"],
+     links: %{"GitHub" => "https://github.com/Puddah/Rendezvous"}]
   end
 end
