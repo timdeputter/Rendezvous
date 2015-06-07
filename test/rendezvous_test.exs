51a40f847d8add078b9b60ff1398fdb22f65cdef
Tim de Putter <tim.de.putter83@googlemail.com>
2015-06-07 16:38:26 +0200

    Refactoring

--- test/rendezvous_test.exs
+++ test/rendezvous_test.exs
@@ -1,5 +1,8 @@
 defmodule RendezvousTest do
   use ExUnit.Case
+  import Enum
+  import Rendezvous
+  import List, only: [delete: 2]
 
   setup do
     buckets = ["Node A", "Node B", "Node C"]
@@ -22,15 +25,15 @@ defmodule RendezvousTest do
   end
 
   test "When a buckets fails only the objects in that bucket have to be moved", context do
-    bucketsWithoutB = List.delete(context[:buckets], "Node B")
+    bucketsWithoutB = delete(context[:buckets], "Node B")
     assert get_hrwbucket("A",bucketsWithoutB) == get_hrwbucket("A",context[:buckets])
     assert get_hrwbucket("AAAF",bucketsWithoutB) == get_hrwbucket("AAAF",context[:buckets])
     assert get_hrwbucket("AA",bucketsWithoutB) != get_hrwbucket("AA",context[:buckets])
-    assert Enum.member? bucketsWithoutB, get_hrwbucket("AA",bucketsWithoutB)
+    assert member? bucketsWithoutB, get_hrwbucket("AA",bucketsWithoutB)
   end
 
   test "Determines for a given key the appropriate node in an elixir cluster" do
-    node = Rendezvous.get_node "A"
+    node = get_node "A"
     assert to_string Node.self == node
   end
 
@@ -64,25 +67,25 @@ defmodule RendezvousTest do
   end
 
   defp get_percentage_of(collection, object) do
-    number_of_eq_to_object = Enum.filter(collection, fn(e) -> e == object end) |> Enum.count
-    result = Float.round(100 / Enum.count(collection) * number_of_eq_to_object,0)
+    number_of_eq_to_object = filter(collection, fn(e) -> e == object end) |> count
+    result = Float.round(100 / count(collection) * number_of_eq_to_object,0)
     result
   end
 
   defp get_buckets(objects,buckets) do
-    Enum.map(objects, fn(object) -> get_hrwbucket(object, buckets) end)
+    map(objects, fn(object) -> get_hrwbucket(object, buckets) end)
   end
 
   defp get_hrwbucket(object,buckets) do
-    Rendezvous.get(:sha, object, buckets)
+    get(:sha, object, buckets)
   end
 
   defp is_bucket(buckets, may_be_bucket) do
-    Enum.any?(buckets, fn e -> e == may_be_bucket end)
+    any?(buckets, fn e -> e == may_be_bucket end)
   end
 
   defp all_equal?(collection) do
-    Enum.uniq(collection) |> Enum.count == 1
+    uniq(collection) |> count == 1
   end
 
 end
