841efed9e794ed73ef38251eb9944ddbd95e7e27
Tim de Putter <tim.de.putter83@googlemail.com>
2015-06-29 21:12:01 +0200

    added dializer

--- lib/rendezvous.ex
+++ lib/rendezvous.ex
@@ -2,11 +2,13 @@ defmodule Rendezvous do
   import Enum
   use FitEx
 
+  @spec get_node(binary) :: binary
   def get_node key do
     nodes = map [Node.self | Node.list], &to_string/1
     get(:sha, key, nodes)
   end
 
+  @spec get(atom, binary, [binary]) :: binary
   def get algorithm, key, buckets do
     {bucket, _hash} = map(buckets, f get_bucket_with_hash(algorithm, it, key)) 
                       |> max_by fn {_b, hash} -> hash end
